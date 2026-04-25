2364127c3425536d2089203f26ae411177971d5e
Ars123455678 <twotservice@yandex.ru>
2026-04-25 18:14:34 +0300

    gjohigjiklehreopbwo;hwgw

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-import json
-
 from flask import Flask, request, jsonify
 import logging
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
+
+# Хранилище сессий
 sessionStorage = {}
 
 
 @app.route('/', methods=['POST'])
-def main1():
-    logging.info(f'Request: {request.json!r}')
-
-    data = json.loads(request.get_json())
+def main():
+    # Flask сам преобразует JSON в словарь, ничего лишнего вызывать не нужно
+    data = request.json
 
     response = {
         'session': data['session'],
@@ -21,48 +20,61 @@ def main1():
     }
 
     handle_dialog(data, response)
-    logging.info(f'Response: {response!r}')
     return jsonify(response)
 
 
 @app.route('/', methods=['GET'])
-def main2():
-    return '''
-        <!DOCTYPE html>
-        <html>
-        <body>
-
-        <h1>My First Heading</h1>
-        <p>My first paragraph.</p>
-
-        </body>
-        </html>
-    '''
+def index():
+    return 'Сервер навыка Алисы запущен!'
 
 
 def handle_dialog(req, res):
+    # Используем .get(), чтобы код не падал, если ключа нет
     user_id = req['session']['user_id']
+
     if req['session']['new']:
-        sessionStorage[user_id] = {'suggests': ["Не хочу.", "Не буду.", "Отстань!"]}
+        sessionStorage[user_id] = {
+            'suggests': ["Не хочу.", "Не буду.", "Отстань!"]
+        }
         res['response']['text'] = 'Привет! Купи слона!'
         res['response']['buttons'] = get_suggests(user_id)
         return
-    if req['request']['original_utterance'].lower() in ['ладно', 'куплю', 'покупаю', 'хорошо', 'я покупаю', 'я куплю']:
+
+    # Обработка ответа пользователя
+    utterance = req['request']['original_utterance'].lower()
+
+    if utterance in ['ладно', 'куплю', 'покупаю', 'хорошо', 'я покупаю', 'я куплю']:
         res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
         res['response']['end_session'] = True
         return
+
+    # Обычный ответ
     res['response']['text'] = f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
     res['response']['buttons'] = get_suggests(user_id)
 
 
 def get_suggests(user_id):
-    session = sessionStorage[user_id]
-    suggests = [{'title': suggest, 'hide': True} for suggest in session['suggests'][:2]]
-    session['suggests'] = session['suggests'][1:]
+    session = sessionStorage.get(user_id, {'suggests': ["Не хочу.", "Не буду.", "Отстань!"]})
+
+    # Формируем кнопки
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    # Обновляем список подсказок
+    if len(session['suggests']) > 1:
+        session['suggests'] = session['suggests'][1:]
+
     sessionStorage[user_id] = session
 
     if len(suggests) < 2:
-        suggests.append({"title": "Ладно", "url": "https://market.yandex.ru/search?text=слон", "hide": True})
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
     return suggests
 
 
